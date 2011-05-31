2269bcc0bf25f524157e218e6df99dad239cae13
tickleman <mc@thgbm.net>
2011-06-01 00:40:17 +0200

    [0.3] display remove result, added /config

--- src/fr/crafter/tickleman/RealAdminTools/RealAdminToolsCommands.java
+++ src/fr/crafter/tickleman/RealAdminTools/RealAdminToolsCommands.java
@@ -1,6 +1,8 @@
 package fr.crafter.tickleman.RealAdminTools;
 
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.FileReader;
 import java.io.FileWriter;
 
 import org.bukkit.World;
@@ -30,6 +32,8 @@ public class RealAdminToolsCommands
 							new FileWriter("plugins/" + plugin.name + "/entities.txt")
 						);
 						writer.write("#class,id,itemTypeId,world,x,y,z\n");
+	    			String what = args.length > 1 ? args[1].toLowerCase() : "";
+						int removed_entities = 0;
 				    for (World world : plugin.getServer().getWorlds()) {
 				    	sender.sendMessage(world.getName() + " :");
 				    	sender.sendMessage("- " + world.getEntities().size() + " entities");
@@ -48,7 +52,6 @@ public class RealAdminToolsCommands
 				    			+ "\n"
 				    		);
 				    		if (subCommand.equals("remove")) {
-				    			String what = args.length > 1 ? args[1].toLowerCase() : "";
 				    			if (
 				    				what.equals(entity.getClass().getName().split(".entity.Craft")[1].toLowerCase())
 				    				|| what.equals("all")
@@ -60,15 +63,72 @@ public class RealAdminToolsCommands
 				    					|| id.equals("all")
 				    				) {
 				    					entity.remove();
+				    					removed_entities ++;
 				    				}
 				    			}
 				    		}
 				    	}
 				    }
+				    if (subCommand.equals("remove")) {
+				    	sender.sendMessage("removed " + removed_entities + " " + what);
+				    }
 						writer.flush();
 						writer.close();
 					} catch (Exception e) {
 						plugin.log.severe("Could not save plugins/" + plugin.name + "/entities.txt file");
+					}
+					return true;
+				}
+				if (command.equals("config")) {
+					String option = args.length > 0 ? args[0].toLowerCase() : "";
+					String value  = args.length > 1 ? args[1].toLowerCase() : ""; 
+					if (!option.equals("") && !value.equals("")) {
+						boolean did_it = false;
+						String bufferOut = "";
+						try {
+							BufferedReader reader = new BufferedReader(
+								new FileReader("server.properties")
+							);
+							String buffer;
+							while ((buffer = reader.readLine()) != null) {
+								if (!buffer.equals("") && buffer.charAt(0) != '#') {
+									if (
+										option.equals(buffer.substring(0, Math.min(buffer.length(), option.length())))
+									) {
+										sender.sendMessage(option + "=" + value);
+										buffer = option + "=" + value;
+										did_it = true;
+									}
+								}
+								if (!bufferOut.equals("")) {
+									bufferOut = bufferOut + "\n";
+								}
+								bufferOut = bufferOut + buffer;
+							}
+							reader.close();
+						} catch (Exception e) {
+							plugin.log.severe("Could not read server.properties file");
+							sender.sendMessage("Could not read server.properties file");
+							bufferOut = "";
+						}
+						if (!bufferOut.equals("")) {
+							if (did_it) {
+								try {
+									BufferedWriter writer = new BufferedWriter(
+										new FileWriter("server.properties")
+									);
+									writer.write(bufferOut);
+									writer.close();
+									sender.sendMessage("server.properties changed : use /reload command to load the new configuration");
+								} catch (Exception e) {
+									plugin.log.severe("Could not write server.properties file");
+									sender.sendMessage("Could not write server.properties file");
+								}
+							} else {
+								sender.sendMessage("Unknown option " + option);
+							}
+						}
+						return true;
 					}
 				}
 			}
--- src/fr/crafter/tickleman/RealAdminTools/RealAdminToolsPlugin.java
+++ src/fr/crafter/tickleman/RealAdminTools/RealAdminToolsPlugin.java
@@ -12,7 +12,7 @@ public class RealAdminToolsPlugin extends RealPlugin
 	//-------------------------------------------------------------------------- RealTeleporterPlugin
 	public RealAdminToolsPlugin()
 	{
-		super("tickleman", "RealAdminTools", "0.02");
+		super("tickleman", "RealAdminTools", "0.03");
 	}
 
 	//------------------------------------------------------------------------------- onPlayerCommand
